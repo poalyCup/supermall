9bd74137b28af72af24e5915acdbc3978be94f24
海哥 <power_aa@yeah.net>
2020-07-03 00:13:10 +0800

    分类菜单栏的布局

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ Vue.use(VueRouter)
   const routes = [
     {
       path: '',
-      redirect: '/cart'
+      redirect: '/category'
     },
     {
       path:'/home',
---
+++ src/views/category/childComponent/CategoryMenuBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-scroll class="menu-bar">
+    <ul>
+      <li v-for="(item, index) in menuList" :key="index" 
+          :class="{ active : index === currentIndex }" @click="menuItemClick(index)">
+        <span>{{ item.title }}</span>
+      </li>
+    </ul>
+  </b-scroll>
+</template>
+
+<script>
+import BScroll from 'components/common/scroll/BScroll'
+
+  export default {
+    name: 'CategoryMenuBar',
+    data(){
+      return{
+        currentIndex: 0
+      }
+    },
+    components: {
+      BScroll
+    },
+    props:{
+      menuList: {
+        type: Array,
+        default(){
+          return []
+        }
+      }
+    },
+    methods: {
+      menuItemClick(index){
+        if(index === this.currentIndex) return
+        this.currentIndex = index
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .menu-bar{
+    width: 80px;
+    height: calc(100vh - 49px);
+  }
+  .menu-bar li{
+    height: 50px;
+    line-height: 50px;
+    background-color: rgba(200, 200, 200, .3);
+  }
+  .menu-bar li span{
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+  }
+  .active span{
+    background-color: #fff;
+  }
+</style>
